388b200e421e011a027585009036ae76c991f4a6
Sagar Khatri <khatrisagar2@gmail.com>
2026-01-09 17:40:31 +0530

    Initial commit

---
+++ app/src/main/java/com/finalyearapp/MainActivity.java
@@ -0,0 +1,67 @@
+package com.finalyearapp;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button login;
+    EditText email,password;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        email = findViewById(R.id.main_email);
+        password = findViewById(R.id.main_password);
+
+        login = findViewById(R.id.main_sign_in);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(email.getText().toString().trim().equals("")){
+                    email.setError("Email Id Required");
+                }
+                else if(!email.getText().toString().trim().matches(emailPattern)){
+                    email.setError("Valid Email Id Required");
+                }
+                else if(password.getText().toString().trim().equals("")){
+                    password.setError("Password Required");
+                }
+                else if(password.getText().toString().trim().length()<6){
+                    password.setError("Valid Password Required");
+                }
+                else {
+                    System.out.println("Login Successfully");
+                    Log.d("LOGIN", "Login Successfully");
+                    Log.e("LOGIN", "Login Successfully");
+                    Log.w("LOGIN", "Login Successfully");
+
+                    Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                    Snackbar.make(view, "Login Successfully", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+}
